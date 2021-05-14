f2fa2b401231f8dc105f1071c5a9cce8f2362e1f
Emma Paris <32328715+vinaya1310@users.noreply.github.com>
2021-05-14 15:59:09 +0530

    flagger app

---
+++ main.py
@@ -0,0 +1,175 @@
+from flask import Flask,render_template,request
+from flasgger import Swagger
+import pickle
+import pandas as pd
+import re
+
+app = Flask(__name__)
+Swagger(app)
+
+from nltk.stem import WordNetLemmatizer
+# Defining dictionary containing all emojis with their meanings.
+emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad', 
+          ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised',
+          ':-@': 'shocked', ':@': 'shocked',':-$': 'confused', ':\\': 'annoyed', 
+          ':#': 'mute', ':X': 'mute', ':^)': 'smile', ':-&': 'confused', '$_$': 'greedy',
+          '@@': 'eyeroll', ':-!': 'confused', ':-D': 'smile', ':-0': 'yell', 'O.o': 'confused',
+          '<(-_-)>': 'robot', 'd[-_-]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink', 
+          ';-)': 'wink', 'O:-)': 'angel','O*-)': 'angel','(:-D': 'gossip', '=^.^=': 'cat'}
+
+## Defining set containing all stopwords in english.
+stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from', 
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're',
+             's', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those', 
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours', 'yourself', 'yourselves']
+def preprocess(textdata):
+    processedText = []
+    
+    # Create Lemmatizer and Stemmer.
+    wordLemm = WordNetLemmatizer()
+    
+    # Defining regex patterns.
+    urlPattern        = r"((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)"
+    userPattern       = '@[^\s]+'
+    alphaPattern      = "[^a-zA-Z0-9]"
+    sequencePattern   = r"(.)\1\1+"
+    seqReplacePattern = r"\1\1"
+    
+    for tweet in textdata:
+        tweet = tweet.lower()
+        
+        # Replace all URls with 'URL'
+        tweet = re.sub(urlPattern,' URL',tweet)
+        # Replace all emojis.
+        for emoji in emojis.keys():
+            tweet = tweet.replace(emoji, "EMOJI" + emojis[emoji])        
+        # Replace @USERNAME to 'USER'.
+        tweet = re.sub(userPattern,' USER', tweet)        
+        # Replace all non alphabets.
+        tweet = re.sub(alphaPattern, " ", tweet)
+        # Replace 3 or more consecutive letters by 2 letter.
+        tweet = re.sub(sequencePattern, seqReplacePattern, tweet)
+
+        tweetwords = ''
+        for word in tweet.split():
+            # Checking if the word is a stopword.
+            #if word not in stopwordlist:
+            if len(word)>1:
+                # Lemmatizing the word.
+                word = wordLemm.lemmatize(word)
+                tweetwords += (word+' ')
+            
+        processedText.append(tweetwords)
+        
+    return processedText
+
+def load_models():
+    # Load the vectoriser.
+    file = open('vectoriser-ngram-(1,2).pickle', 'rb')
+    vectoriser = pickle.load(file)
+    file.close()
+    # Load the LR Model.
+    file = open('Sentiment-LR.pickle', 'rb')
+    LRmodel = pickle.load(file)
+    file.close()
+    
+    return vectoriser, LRmodel
+
+def predict(vectoriser, model, text):
+    # Predict the sentiment
+    textdata = vectoriser.transform(preprocess(text))
+    sentiment = model.predict(textdata)
+    
+    # Make a list of text with sentiment.
+    data = []
+    for text, pred in zip(text, sentiment):
+        data.append((text,pred))
+        
+    # Convert the list into a Pandas DataFrame.
+    df = pd.DataFrame(data, columns = ['text','sentiment'])
+    df = df.replace([0,1], ["Negative","Positive"])
+    return df
+
+if __name__=="__main__":
+    # Loading the models.
+    vectoriser, LRmodel = load_models()
+    
+    # Text to classify should be in a list.
+    text = ["I hate twitter",
+            "May the Force be with you.",
+            "Mr. Stark, I don't feel so good"]
+
+
+@app.route("/")
+def base_route():
+    return "Welcome to praxis"
+   
+# @app.route("/hello",methods = ['GET'])
+# def hello():
+#     """Lets Create some Swagger App
+#     -------
+#     parameters:
+#     -   name: number1
+#         in: query
+#         type: number
+#         required: true
+#     -   name: name1
+#         in: query
+#         type: string
+#         required: true
+#     responses:
+#         200 :
+#             description : the result is
+
+#     """
+#     number1 = request.args.get("number1")
+#     name1 = request.args.get("name1")
+#     return f"The Number is {number1} and the name is {name1}"
+
+# if __name__ == "__main__":
+#     app.run(debug= True , host="127.0.0.1", port=2000)
+
+def predict_using_text(text):
+    df = predict(vectoriser, LRmodel, text)
+    return(df.head())
+ 
+
+@app.route("/hello",methods = ['GET'])
+def hello():
+    """Lets Create some Swagger App
+    -------
+    parameters:
+    -   name: Tweet
+        in: query
+        type: string
+        required: true
+    responses:
+        200 :
+            description : the result is
+
+    """
+    tweet_sentiment = request.args.get("Tweet")
+    df1 = predict_using_text([tweet_sentiment])
+    print(df1.head())
+    return f"The sentiment is {df1.head()}"
+
+if __name__ == "__main__":
+    app.run(debug= True , host="127.0.0.1", port=2000)
+
+
+
+
+#df1 = predict_using_text(['I am awesome'])
+#print(df1.head())
